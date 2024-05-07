5f81ac896e6657afc0f3c7112c13019f1a0d8c67
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-07 11:41:00 +0900

    Create : DBManager class getConnection method

--- src/main/java/rentcarServer/util/DBManager.java
+++ src/main/java/rentcarServer/util/DBManager.java
@@ -1,6 +1,28 @@
 package rentcarServer.util;
 
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class DBManager {
 
-	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Context init = new InitialContext();
+			DataSource source = (DataSource) init.lookup("java:comp/env/jdbc/rantcarServerDB");
+		
+			conn = source.getConnection();
+			
+			System.out.println("[DB 연동 성공]");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("[DB 연동 실패]");
+		}
+		
+		return conn;
+	}
 }
