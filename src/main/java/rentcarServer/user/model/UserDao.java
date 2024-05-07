4486bd1dac8ec23cb409135fbcf23bce3e68aca1
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-08 00:09:47 +0900

    Create : UserDao class findUserByIdAndPassword method 생성

--- src/main/java/rentcarServer/user/model/UserDao.java
+++ src/main/java/rentcarServer/user/model/UserDao.java
@@ -1,6 +1,18 @@
 package rentcarServer.user.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import rentcarServer.user.model.UserRequestDto;
+import rentcarServer.util.DBManager;
+import rentcarServer.util.PasswordCrypto;
+
 public class UserDao {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 	
 	// UserDao 객체를 단일 인스턴스로 만들기 위해
 	// Singleton Pattern 적용
@@ -16,5 +28,46 @@ public class UserDao {
 	// 3. 단일 인스턴스에 대한 getter
 	public static UserDao getInstance() {
 		return instance;
+	}
+	
+	public UserResponseDto findUserByIdAndPassword(String id, String password) {
+		UserResponseDto user = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT user_id, password, email, name, birth, gender, country, telecom, phone FROM users WHERE user_id=?";
+		
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,  id);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String encryptedPassword = rs.getString(2);
+				String email = rs.getString(3);
+				String name = rs.getString(4);
+				String birth = rs.getString(5);
+				String gender = rs.getString(6);
+				String country = rs.getString(7);
+				String telecom = rs.getString(8);
+				String phone = rs.getString(9);
+				
+				if(PasswordCrypto.decrypt(password, encryptedPassword)) {
+					user = new UserResponseDto(id, email, name, birth, gender, country, telecom, phone);
+					return user;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return null;
+	}
+	
+	public boolean userExists(UserRequestDto userDto) {
+		return findUserByIdAndPassword(userDto.getId(), userDto.getPassword()) != null;
 	}
 }
