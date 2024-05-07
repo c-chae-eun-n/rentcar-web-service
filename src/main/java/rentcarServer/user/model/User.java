a8c163edb676d0db3506ff8ea1d84d08674e77a3
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-07 18:33:50 +0900

    Delete : User class agree variable

--- src/main/java/rentcarServer/user/model/User.java
+++ src/main/java/rentcarServer/user/model/User.java
@@ -11,12 +11,11 @@ public class User {
 	private String country;
 	private String telecom;
 	private String phone;
-	private boolean agree;
 	private Timestamp regDate;
 	private Timestamp modDate;
 
 	public User(String id, String email, String name, String birth, String gender, String country, String telecom,
-			String phone, boolean agree, Timestamp regDate, Timestamp modDate) {
+			String phone, Timestamp regDate, Timestamp modDate) {
 		super();
 		this.id = id;
 		this.email = email;
@@ -26,7 +25,6 @@ public class User {
 		this.country = country;
 		this.telecom = telecom;
 		this.phone = phone;
-		this.agree = agree;
 		this.regDate = regDate;
 		this.modDate = modDate;
 	}
@@ -61,10 +59,6 @@ public class User {
 
 	public String getPhone() {
 		return phone;
-	}
-
-	public boolean isAgree() {
-		return agree;
 	}
 
 	public Timestamp getRegDate() {
