8530fb098e0c4925dfeb420aa0f60826f91ecc2e
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-08 11:37:15 +0900

    Create : JoinFormAction 사용자 입력값 재검사 후 사용자 생성하는 POST 메소드 생성

--- src/main/java/rentcarServer/user/controller/JoinFormAction.java
+++ src/main/java/rentcarServer/user/controller/JoinFormAction.java
@@ -1,5 +1,86 @@
 package rentcarServer.user.controller;
 
-public class JoinFormAction {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import rentcarServer.user.model.UserDao;
+import rentcarServer.user.model.UserRequestDto;
+import rentcarServer.user.model.UserResponseDto;
+
+/**
+ * Servlet implementation class JoinFormAction
+ */
+@WebServlet("/JoinFormAction")
+public class JoinFormAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JoinFormAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+		String birth = request.getParameter("birth");
+		String gender = request.getParameter("gender");
+		String country = request.getParameter("country");
+		String telecom = request.getParameter("telecom");
+		String phone = request.getParameter("phone");
+
+		boolean isValid = true;
+		
+		if(id == null || id.equals(""))
+			isValid = false;
+		else if(password == null || password.equals(""))
+			isValid = false;
+		else if(name == null || name.equals(""))
+			isValid = false;
+		else if(birth == null || birth.equals(""))
+			isValid = false;
+		else if(gender == null || gender.equals(""))
+			isValid = false;
+		else if(country == null || country.equals(""))
+			isValid = false;
+		else if(telecom == null || telecom.equals(""))
+			isValid = false;
+		else if(phone == null || phone.equals(""))
+			isValid = false;
+		
+		if(isValid) {
+			UserRequestDto userDto = new UserRequestDto(id, password, email, name, birth, gender, country, telecom, phone);
+			
+			UserDao userDao = UserDao.getInstance();
+			UserResponseDto user = userDao.createUser(userDto);
+
+			if(user == null) {
+				response.sendRedirect("/join");
+			} else {
+				response.sendRedirect("/");
+			}
+		}
+	}
 
 }
