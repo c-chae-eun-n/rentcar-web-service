a1369198858036933bcdbf61502c97a7c734d469
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-08 18:40:36 +0900

    Update : updateUserPassword, updateUserEmail, updateUserPhone method 추가

--- src/main/java/rentcarServer/user/model/UserDao.java
+++ src/main/java/rentcarServer/user/model/UserDao.java
@@ -3,9 +3,9 @@ package rentcarServer.user.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
-import rentcarServer.user.model.UserRequestDto;
 import rentcarServer.util.DBManager;
 import rentcarServer.util.PasswordCrypto;
 
@@ -15,18 +15,12 @@ public class UserDao {
 	private PreparedStatement pstmt;
 	private ResultSet rs;
 	
-	// UserDao 객체를 단일 인스턴스로 만들기 위해
-	// Singleton Pattern 적용
-	
-	// 1. 생성자를 private으로
 	private UserDao() {
 //		setConnection();
 	}
 	
-	// 2. 단일 인스턴스를 생성 (클래스 내부에서)
 	private static UserDao instance = new UserDao();
 	
-	// 3. 단일 인스턴스에 대한 getter
 	public static UserDao getInstance() {
 		return instance;
 	}
@@ -78,6 +72,7 @@ public class UserDao {
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
+				System.out.println("rsnext");
 				String encryptedPassword = rs.getString(2);
 				String email = rs.getString(3);
 				String name = rs.getString(4);
@@ -142,5 +137,104 @@ public class UserDao {
 	
 	public boolean userExists(String id) {
 		return findUserById(id) != null;
+	}
+	
+	public UserResponseDto updateUserPassword(UserRequestDto userDto, String newPassword) {
+		UserResponseDto user = null;
+		
+		if(newPassword == null || newPassword.equals("")) {
+			return user;
+		}
+
+		System.out.println("find password : ");
+		if(findUserByIdAndPassword(userDto.getId(), userDto.getPassword()) == null)
+			return user;
+		
+		try {
+			System.out.println("password : ");
+			conn = DBManager.getConnection();
+			
+			String sql = "UPDATE users SET password=? WHERE user_id=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, PasswordCrypto.encrypt(newPassword));
+			pstmt.setString(2, userDto.getId());
+			
+			pstmt.execute();
+			
+			User userVo = findUserById(userDto.getId());
+			
+			user = new UserResponseDto(userVo);
+			
+			return user;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+		
+		return user;
+	}
+	
+	public UserResponseDto updateUserEmail(UserRequestDto userDto) {
+		UserResponseDto user = null;
+		
+		System.out.println("find email : ");
+		
+		if(findUserByIdAndPassword(userDto.getId(), userDto.getPassword()) == null)
+			return user;
+		
+		try {
+			System.out.println("email : ");
+			conn = DBManager.getConnection();
+			
+			String sql = "UPDATE users SET email=? WHERE user_id=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userDto.getEmail());
+			pstmt.setString(2, userDto.getId());
+			
+			pstmt.execute();
+			
+			user = findUserByIdAndPassword(userDto.getId(), userDto.getPassword());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+		
+		return user;
+	}
+	
+	public UserResponseDto updateUserPhone(UserRequestDto userDto) {
+		UserResponseDto user = null;
+
+		System.out.println("find phone : ");
+		if(findUserByIdAndPassword(userDto.getId(), userDto.getPassword()) == null)
+			return user;
+		
+		try {
+			System.out.println("phone : ");
+			conn = DBManager.getConnection();
+			
+			String sql = "UPDATE users SET telecom=?, phone=? WHERE user_id=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userDto.getTelecom());
+			pstmt.setString(2, userDto.getPhone());
+			pstmt.setString(3, userDto.getId());
+			
+			pstmt.execute();
+			
+			user = findUserByIdAndPassword(userDto.getId(), userDto.getPassword());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+		
+		return user;
 	}
 }
