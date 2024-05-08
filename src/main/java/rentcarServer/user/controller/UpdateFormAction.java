78c4418d2f42dc89b822bd19bcd72fbe0883f0ea
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-08 18:40:58 +0900

    Update : UpdateFormAction

--- src/main/java/rentcarServer/user/controller/UpdateFormAction.java
+++ src/main/java/rentcarServer/user/controller/UpdateFormAction.java
@@ -6,6 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import rentcarServer.user.model.UserDao;
+import rentcarServer.user.model.UserRequestDto;
+import rentcarServer.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class UpdateFormAction
@@ -34,8 +39,51 @@ public class UpdateFormAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+
+		UserDao userDao = UserDao.getInstance();
+		
+		// session에 저장되어있는 타입
+		UserResponseDto user = (UserResponseDto) session.getAttribute("user");
+
+		String password = request.getParameter("password");
+		
+		// 입력된 패스워드 검증 후
+		if(userDao.findUserByIdAndPassword(user.getId(), password) != null) {
+			UserRequestDto userDto = new UserRequestDto();
+			
+			userDto.setId(user.getId());
+			userDto.setPassword(password);
+			
+			String newPassword = request.getParameter("password-new");
+			String email = request.getParameter("email");
+			
+			String telecom = request.getParameter("telecom");
+			String phone = request.getParameter("phone");
+			
+			if(!email.equals((user.getEmail() == null || user.getEmail().equals("")) ? "" : user.getEmail())) {
+				userDto.setEmail(email);
+				
+				// 변경된 내역을 user에 담아줌
+				user = userDao.updateUserEmail(userDto);
+			}
+			
+			if(!telecom.equals(user.getTelecom()) || !phone.equals(user.getPhone())) {
+				userDto.setTelecom(telecom);
+				userDto.setPhone(phone);
+				user = userDao.updateUserPhone(userDto);
+			}
+			
+			if(!newPassword.equals("") && !newPassword.equals(password)) {
+				userDao.updateUserPassword(userDto, newPassword);
+			}
+		}
+		
+		// 갱신 된 내용을 세션에 반영
+		session.setAttribute("user", user);
+		response.sendRedirect("/mypage");
 	}
 
 }
