1fe82f55c5c244ac38e8362162adb05acd0f92dd
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-08 15:07:05 +0900

    Create : LoginFormAction 생성

--- src/main/java/rentcarServer/user/controller/LoginFormAction.java
+++ src/main/java/rentcarServer/user/controller/LoginFormAction.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import rentcarServer.user.model.UserDao;
+import rentcarServer.user.model.UserResponseDto;
 
 /**
  * Servlet implementation class LoginFormAction
@@ -34,8 +38,30 @@ public class LoginFormAction extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		boolean isValid = true;
+		
+		if(id == null || id.equals(""))
+			isValid = false;
+		else if(password == null || password.equals(""))
+			isValid = false;
+		
+		if(isValid) {
+			UserDao userDao = UserDao.getInstance();
+			UserResponseDto user = userDao.findUserByIdAndPassword(id, password);
+			
+			HttpSession session = request.getSession();
+
+			if(user != null) {
+				session.setAttribute("user", user);
+				System.out.println("session.get: "+session.getAttribute("user"));
+				response.sendRedirect("/");
+			} else {
+				response.sendRedirect("/login");
+			}
+		}
 	}
 
 }
