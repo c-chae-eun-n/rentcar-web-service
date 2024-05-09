06fac1b0594025414d61e69a272c5860733a6a86
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-09 11:01:55 +0900

    Update : BoardDao class constructor singleton

--- src/main/java/rentcarServer/board/model/BoardDao.java
+++ src/main/java/rentcarServer/board/model/BoardDao.java
@@ -2,4 +2,14 @@ package rentcarServer.board.model;
 
 public class BoardDao {
 
+	private BoardDao() {
+		
+	}
+	
+	private static BoardDao instance = new BoardDao();
+	
+	public static BoardDao getInstance() {
+		return instance;
+	}
+	
 }
