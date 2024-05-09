7f84c5cf1c0b77034665d2cd917894571167fc58
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-09 11:33:32 +0900

    Update : BoardResponseDto constructor 생성

--- src/main/java/rentcarServer/board/model/BoardResponseDto.java
+++ src/main/java/rentcarServer/board/model/BoardResponseDto.java
@@ -6,5 +6,23 @@ public class BoardResponseDto {
 	private String content;
 	private String userId;
 	private String category;
+	
+	public BoardResponseDto(String code, String title, String content, String userId, String category) {
+		super();
+		this.code = code;
+		this.title = title;
+		this.content = content;
+		this.userId = userId;
+		this.category = category;
+	}
 
+	public BoardResponseDto(Board board) {
+		this.code = board.getCode();
+		this.title = board.getTitle();
+		this.content = board.getContent();
+		this.userId = board.getUserId();
+		this.category = board.getCategory();
+	}
+	
+	
 }
