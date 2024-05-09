0c23527938cd6da06eacc69ca8262c478579880e
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-09 14:48:17 +0900

    Update : BoardDao createPostCode & findPostCode 메서드 추가

--- src/main/java/rentcarServer/board/model/BoardDao.java
+++ src/main/java/rentcarServer/board/model/BoardDao.java
@@ -1,6 +1,17 @@
 package rentcarServer.board.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Random;
+
+import rentcarServer.util.DBManager;
+
 public class BoardDao {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 
 	private BoardDao() {
 		
@@ -12,4 +23,48 @@ public class BoardDao {
 		return instance;
 	}
 	
+	private boolean findPostCode(String code) {
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT code FROM boards WHERE code=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, code);
+			
+			rs = pstmt.executeQuery();
+			
+			System.out.println("코드 검사 시작");
+			if(rs.next()) {
+				System.out.println("코드 검사 끝");
+				return false;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return true;
+	}
+	
+	public String createPostCode() {
+		String code = "";
+		while(true) {
+			Random random = new Random();
+			
+			int rNum1 = random.nextInt(900000)+100000;
+			int rNum2 = random.nextInt(900000)+100000;
+			code = rNum1 + "-" + rNum2;
+			System.out.println("createCode: "+code);
+			if(findPostCode(code))
+				break;
+		}
+		
+		return code;
+	}
+	
+
 }
