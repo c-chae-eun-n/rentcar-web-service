62b73e0b586fb040ee853b33697b48c18333bce0
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-09 11:22:30 +0900

    Update : BoardRequestDto variable 생성

--- src/main/java/rentcarServer/board/model/BoardRequestDto.java
+++ src/main/java/rentcarServer/board/model/BoardRequestDto.java
@@ -1,5 +1,11 @@
 package rentcarServer.board.model;
 
 public class BoardRequestDto {
-
+	private String code;
+	private String title;
+	private String content;
+	private String userId;
+	private String category;
+	
+	
 }
