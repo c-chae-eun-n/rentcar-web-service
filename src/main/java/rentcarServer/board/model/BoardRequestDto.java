90a0bf7f5e20bc48c01cea43f0c8ad26746ed52d
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-09 11:24:29 +0900

    Update : BoardRequestDto constructor 생성

--- src/main/java/rentcarServer/board/model/BoardRequestDto.java
+++ src/main/java/rentcarServer/board/model/BoardRequestDto.java
@@ -7,5 +7,18 @@ public class BoardRequestDto {
 	private String userId;
 	private String category;
 	
+	public BoardRequestDto() {
+		super();
+	}
+
+	public BoardRequestDto(String code, String title, String content, String userId, String category) {
+		super();
+		this.code = code;
+		this.title = title;
+		this.content = content;
+		this.userId = userId;
+		this.category = category;
+	}
+	
 	
 }
