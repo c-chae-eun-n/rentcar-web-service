a5ca6e943bf7bb37961af3c45afa80a413bad974
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-10 12:47:23 +0900

    Update : singleton constructor

--- src/main/java/rentcarServer/car/model/CarDao.java
+++ src/main/java/rentcarServer/car/model/CarDao.java
@@ -1,5 +1,13 @@
 package rentcarServer.car.model;
 
 public class CarDao {
-
+	private CarDao() {
+		
+	}
+	
+	private static CarDao instance = new CarDao();
+	
+	public static CarDao getInstance() {
+		return instance;
+	}
 }
