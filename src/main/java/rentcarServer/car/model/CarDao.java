195a2d9a2c7fd4435e8417f7379a0bd4c735d667
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-10 17:07:30 +0900

    Add : findCarByCode 메소드 추가

--- src/main/java/rentcarServer/car/model/CarDao.java
+++ src/main/java/rentcarServer/car/model/CarDao.java
@@ -55,4 +55,40 @@ public class CarDao {
 		
 		return carList;
 	}
+	
+	public CarResponseDto findCarByCode(String code) {
+		CarResponseDto car = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT car_code, model, price, car_class, car_number, reservation, fuel FROM cars WHERE car_code=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, code);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String carCode = rs.getString(1);
+				String model = rs.getString(2);
+				String price = rs.getString(3);
+				String carClass = rs.getString(4);
+				String carNumber = rs.getString(5);
+				boolean reservation = rs.getBoolean(6);
+				String fuel = rs.getString(7);
+				
+				if(carCode.equals(code)) {
+					car = new CarResponseDto(carCode, model, price, carClass, carNumber, reservation, fuel);
+					return car;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return car;
+	}
 }
