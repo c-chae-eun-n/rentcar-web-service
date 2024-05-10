0114e1892e871d913f7a62c1735cc7be873b6a7c
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-10 14:41:11 +0900

    Update : CarResponseDto

--- src/main/java/rentcarServer/car/model/CarResponseDto.java
+++ src/main/java/rentcarServer/car/model/CarResponseDto.java
@@ -2,4 +2,52 @@ package rentcarServer.car.model;
 
 public class CarResponseDto {
 
+	private String carCode;
+	private String model;
+	private String price;
+	private String carClass;
+	private String carNumber;
+	private boolean reservation;
+	private String fuel;
+	
+	public CarResponseDto(String carCode, String model, String price, String carClass, String carNumber,
+			boolean reservation, String fuel) {
+		super();
+		this.carCode = carCode;
+		this.model = model;
+		this.price = price;
+		this.carClass = carClass;
+		this.carNumber = carNumber;
+		this.reservation = reservation;
+		this.fuel = fuel;
+	}
+
+	public String getCarCode() {
+		return carCode;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public String getCarClass() {
+		return carClass;
+	}
+
+	public String getCarNumber() {
+		return carNumber;
+	}
+
+	public boolean isReservation() {
+		return reservation;
+	}
+
+	public String getFuel() {
+		return fuel;
+	}
+	
 }
