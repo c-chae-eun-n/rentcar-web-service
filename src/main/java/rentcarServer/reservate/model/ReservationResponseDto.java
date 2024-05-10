7ca78c4aed23d8db807456fc37afa6a01c111fae
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-10 18:00:29 +0900

    Modify : ReservationResponseDto

---
+++ src/main/java/rentcarServer/reservate/model/ReservationResponseDto.java
@@ -0,0 +1,102 @@
+package rentcarServer.reservate.model;
+
+public class ReservationResponseDto {
+	private String number;
+	private String userId;
+	private String carCode;
+	private String renDate;
+	private String returnDate;
+	private String insurance;
+	private boolean paymentStatus;
+	private String payment;
+	
+	public ReservationResponseDto(String number, String userId, String carCode, String renDate, String returnDate,
+			String insurance, boolean paymentStatus, String payment) {
+		super();
+		this.number = number;
+		this.userId = userId;
+		this.carCode = carCode;
+		this.renDate = renDate;
+		this.returnDate = returnDate;
+		this.insurance = insurance;
+		this.paymentStatus = paymentStatus;
+		this.payment = payment;
+	}
+	
+	public ReservationResponseDto(Reservation reservation) {
+		super();
+		this.number = reservation.getNumber();
+		this.userId = reservation.getUserId();
+		this.carCode = reservation.getCarCode();
+		this.renDate = reservation.getRenDate();
+		this.returnDate = reservation.getReturnDate();
+		this.insurance = reservation.getInsurance();
+		this.paymentStatus = reservation.isPaymentStatus();
+		this.payment = reservation.getPayment();
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getCarCode() {
+		return carCode;
+	}
+
+	public void setCarCode(String carCode) {
+		this.carCode = carCode;
+	}
+
+	public String getRenDate() {
+		return renDate;
+	}
+
+	public void setRenDate(String renDate) {
+		this.renDate = renDate;
+	}
+
+	public String getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(String returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public String getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(String insurance) {
+		this.insurance = insurance;
+	}
+
+	public boolean isPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(boolean paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getPayment() {
+		return payment;
+	}
+
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+}
