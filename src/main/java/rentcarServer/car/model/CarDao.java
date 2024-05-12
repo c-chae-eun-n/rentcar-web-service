ae94f7223ba7df1393a3de85e40a5163a3e70689
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-13 01:40:17 +0900

    Update : readSearchCar, searchCarByModel 지역, 날짜 검색 메소드 & 모델명 검색 메소드 추가

--- src/main/java/rentcarServer/car/model/CarDao.java
+++ src/main/java/rentcarServer/car/model/CarDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import rentcarServer.reservate.model.ReservationResponseDto;
 import rentcarServer.util.DBManager;
 
 public class CarDao {
@@ -45,8 +46,74 @@ public class CarDao {
 				String location = rs.getString(8);
 				String seater = rs.getString(9);
 				
-				CarResponseDto board = new CarResponseDto(code, model, price, carClass, carNumber, reservation, fuel, location, seater);
-				carList.add(board);
+				CarResponseDto car = new CarResponseDto(code, model, price, carClass, carNumber, reservation, fuel, location, seater);
+				carList.add(car);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		System.out.println("all carList size: " + carList.size());
+		return carList;
+	}
+	
+	public List<CarResponseDto> readSearchCar(List<ReservationResponseDto> reservationList, String loca) {
+		List<CarResponseDto> carList = readAllCar();
+		
+		for(int i=0; i<reservationList.size(); i++) {
+			for(int j=0; j<carList.size(); j++) {
+				if(reservationList.get(i).getCarCode().equals(carList.get(j).getCarCode())) {
+					carList.remove(j);
+					j --;
+					System.out.println("remove carList size: " + carList.size());
+				}
+			}
+		}
+		if(loca.equals("all")) {
+			System.out.println("loca is all");
+			return carList;
+		}
+		System.out.println("loca: " + loca);
+		for(int i=0; i<carList.size(); i++) {
+			System.out.println("carList.get(i).getLocation(): " + carList.get(i).getLocation());
+			
+			if(!carList.get(i).getLocation().equals(loca)) {
+				System.out.println("equals Location: " + carList.get(i).getLocation());
+				carList.remove(i);
+				i--;
+			}
+		}
+		
+		return carList;
+	}
+	
+	public List<CarResponseDto> searchCarByModel(String carModel){
+		List<CarResponseDto> carList = new ArrayList<CarResponseDto>();
+		
+		try {
+			conn = DBManager.getConnection();
+			String sql = "SELECT * FROM cars WHERE model LIKE ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%"+carModel+"%");
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String code = rs.getString(1);
+				String model = rs.getString(2);
+				String price = rs.getString(3);
+				String carClass = rs.getString(4);
+				String carNumber = rs.getString(5);
+				boolean reservation = rs.getBoolean(6);
+				String fuel = rs.getString(7);
+				String location = rs.getString(8);
+				String seater = rs.getString(9);
+				
+				CarResponseDto car = new CarResponseDto(code, model, price, carClass, carNumber, reservation, fuel, location, seater);
+				carList.add(car);
 			}
 			
 		} catch (Exception e) {
