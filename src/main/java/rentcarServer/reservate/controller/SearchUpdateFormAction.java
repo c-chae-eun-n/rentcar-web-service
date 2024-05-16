9f0dd15a030bd5e4bd234eb00e5b1ebfaf294f22
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-16 23:25:44 +0900

    Modify : SearchUpdateFormAction 예약 내역 전달해주는 servlet 추가

---
+++ src/main/java/rentcarServer/reservate/controller/SearchUpdateFormAction.java
@@ -0,0 +1,70 @@
+package rentcarServer.reservate.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import rentcarServer.reservate.model.ReservationDao;
+import rentcarServer.reservate.model.ReservationResponseDto;
+
+/**
+ * Servlet implementation class SearchUpdateFormAction
+ */
+public class SearchUpdateFormAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchUpdateFormAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+		
+		String number = request.getParameter("number");
+		System.out.println(number);
+		
+		ReservationDao reservationDao = ReservationDao.getInstance();
+		
+		System.out.println(number);
+		
+		ReservationResponseDto reserve = reservationDao.searchReservationByNumber(number);
+		
+		String[] tempRenDate = reservationDao.convertTimeStampToString(reserve.getRenDate(), "yyyy-MM-dd HH:mm:ss").split(" ");
+		String[] tempReturnDate = reservationDao.convertTimeStampToString(reserve.getReturnDate(), "yyyy-MM-dd HH:mm:ss").split(" ");
+		
+		String renDate = tempRenDate[0];
+		String renTime = tempRenDate[1];
+		String returnDate = tempReturnDate[0];
+		String returnTime = tempReturnDate[1];
+		
+		session.setAttribute("renDate", renDate);
+		session.setAttribute("renTime", renTime);
+		session.setAttribute("returnDate", returnDate);
+		session.setAttribute("returnTime", returnTime);
+
+		request.setAttribute("reserve", reserve);
+
+		request.getRequestDispatcher("/updateReservaion").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
